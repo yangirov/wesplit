fc85aaf834be76e02da1b3db75730564c4527b8a
Emil Yangirov <emil@yangirov.ru>
2022-01-30 18:29:41 +0500

    fix: Landing localization

--- src/app/landing/landing.module.ts
+++ src/app/landing/landing.module.ts
@@ -4,9 +4,15 @@ import { CommonModule } from '@angular/common';
 import { LandingRoutingModule } from './landing-routing.module';
 import { LandingPageComponent } from './landing-page/landing-page.component';
 import { MatButtonModule } from '@angular/material/button';
+import { TranslocoModule } from '@ngneat/transloco';
 
 @NgModule({
   declarations: [LandingPageComponent],
-  imports: [CommonModule, LandingRoutingModule, MatButtonModule],
+  imports: [
+    CommonModule,
+    LandingRoutingModule,
+    MatButtonModule,
+    TranslocoModule,
+  ],
 })
 export class LandingModule {}
