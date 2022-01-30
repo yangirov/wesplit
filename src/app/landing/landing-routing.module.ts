f981b928664e42872b3c5d4f88a5736472b0068b
Emil Yangirov <emil@yangirov.ru>
2022-01-30 14:50:10 +0500

    fix: Redirect to event page if is PWA mode

--- src/app/landing/landing-routing.module.ts
+++ src/app/landing/landing-routing.module.ts
@@ -1,11 +1,27 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LandingPageComponent } from './landing-page/landing-page.component';
+import { AuthGuard, isNotAnonymous } from '@angular/fire/auth-guard';
+import isPwa from '../../utils/PwaExtensions';
+import { pipe } from 'rxjs';
+import { map } from 'rxjs/operators';
+
+export const redirectToEventsIfIsPwa = (redirect: any[]) =>
+  pipe(
+    isNotAnonymous,
+    map((loggedIn) => !loggedIn || (isPwa() && redirect) || true)
+  );
+
+const redirectLoggedInToEvents = () => redirectToEventsIfIsPwa(['events']);
 
 const routes: Routes = [
   {
     path: '',
     component: LandingPageComponent,
+    canActivate: [AuthGuard],
+    data: {
+      authGuardPipe: redirectLoggedInToEvents,
+    },
   },
 ];
 
