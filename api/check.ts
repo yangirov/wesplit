bbb77544c59e555f65841223ac185f2943219e1b
Emil Yangirov <emil@yangirov.ru>
2022-02-25 16:00:10 +0500

    chore: Security leaks

--- api/check.ts
+++ api/check.ts
@@ -7,12 +7,12 @@ const firebaseAdmin = require('firebase-admin');
 
 const serviceAccount = {
   type: 'service_account',
-  project_id: process.env.FIREBASE_PROJECT_ID,
-  private_key_id: process.env.FIREBASE_PRIVATE_KEY_ID,
-  private_key: process.env.FIREBASE_PRIVATE_KEY,
-  client_email: process.env.FIREBASE_CLIENT_EMAIL,
-  client_id: process.env.FIREBASE_CLIENT_ID,
-  client_x509_cert_url: process.env.FIREBASE_CLIENT_CERT_URL,
+  project_id: process.env.NG_APP_FIREBASE_PROJECT_ID,
+  private_key_id: process.env.NG_APP_FIREBASE_PRIVATE_KEY_ID,
+  private_key: process.env.NG_APP_FIREBASE_PRIVATE_KEY,
+  client_email: process.env.NG_APP_FIREBASE_CLIENT_EMAIL,
+  client_id: process.env.NG_APP_FIREBASE_CLIENT_ID,
+  client_x509_cert_url: process.env.NG_APP_FIREBASE_CLIENT_CERT_URL,
   auth_uri: 'https://accounts.google.com/o/oauth2/auth',
   token_uri: 'https://oauth2.googleapis.com/token',
   auth_provider_x509_cert_url: 'https://www.googleapis.com/oauth2/v1/certs',
---
+++ src/env.d.ts
@@ -0,0 +1,6 @@
+declare var process: {
+  env: {
+    NG_APP_ENV: string;
+    [key: string]: any;
+  };
+};
