282413e20526dd9df3c3ac9c3972c68dc22b463f
Emil Yangirov <emil@yangirov.ru>
2022-08-10 11:54:49 +0500

    chore: Prettier

--- src/models/Receipt.ts
+++ src/models/Receipt.ts
@@ -1,16 +1,16 @@
 export interface Receipt {
-  user: string,
-  totalSum: number,
-  date: string,
-  retailPlace: string,
-  retailPlaceAddress: string,
-  items: ReceiptPurchase[]
+  user: string;
+  totalSum: number;
+  date: string;
+  retailPlace: string;
+  retailPlaceAddress: string;
+  items: ReceiptPurchase[];
 }
 
 export interface ReceiptPurchase {
-  name: string,
-  price: number,
-  sum: number,
-  quantity: number,
-  selected: boolean
+  name: string;
+  price: number;
+  sum: number;
+  quantity: number;
+  selected: boolean;
 }
